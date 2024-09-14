69e17e2dfcdb7ebaad62821e179bb880eb942e42
Lucas Gregory <124362765+oLucasGregory@users.noreply.github.com>
2024-09-14 12:46:52 +0100

    implementando styled compones

--- src/components/Vaga/styles.ts
+++ src/components/Vaga/styles.ts
@@ -1,18 +1,27 @@
-.vaga {
+import styled from 'styled-components'
+
+export const VagaLi = styled.li`
   border: 1px solid var(--cor-principal);
   background-color: var(--cor-secundaria);
   color: var(--cor-principal);
   padding: 16px;
   transition: all ease 0.3s;
   border-radius: 8px;
-}
-
-.vagaTitulo {
+  &:hover {
+    background-color: var(--cor-principal);
+    color: var(--cor-secundaria);
+  }
+  &:hover a {
+    border-color: var(--cor-principal);
+    background-color: var(--cor-secundaria);
+    color: var(--cor-principal);
+  }
+`
+export const VagaTitulo = styled.h3`
   font-weight: bold;
   margin-bottom: 16px;
-}
-
-.vagaLink {
+`
+export const VagaLink = styled.a`
   border-color: var(--cor-secundaria);
   background-color: var(--cor-principal);
   color: var(--cor-secundaria);
@@ -24,19 +33,7 @@
   font-size: 14px;
   border-radius: 8px;
   text-align: center;
-
   @media (max-width: 768px) {
     display: block;
   }
-}
-
-.vaga:hover {
-  background-color: var(--cor-principal);
-  color: var(--cor-secundaria);
-}
-
-.vaga:hover a {
-  border-color: var(--cor-principal);
-  background-color: var(--cor-secundaria);
-  color: var(--cor-principal);
-}
+`
--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,7 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const EstiloGlobal = createGlobalStyle`
+
 :root {
   --cor-principal: #a7727d;
   --cor-secundaria: #f9f5e7;
@@ -14,15 +18,12 @@
 body {
   padding-bottom: 120px;
 }
-
-.container {
+`
+export const Container = styled.div`
   max-width: 1024px;
   width: 100%;
   margin: 0 auto;
-}
-
-@media (max-width: 1024px) {
-  .container {
+  @media (max-width: 1024px) {
     max-width: 80%;
   }
-}
+`
